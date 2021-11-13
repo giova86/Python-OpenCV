1fe2bdb68b889f52edc3ba701905aedf3d4042a7
giova86 <giovanni.bocchi@gmail.com>
2021-11-13 10:17:28 +0100

    mediapipe tutorial

--- HandTrackingMin.py
+++ HandTrackingMin.py
@@ -19,13 +19,20 @@ while True:
 
     if results.multi_hand_landmarks:
         for handLms in results.multi_hand_landmarks:
+            for id, lm in enumerate(handLms.landmark):
+                # print(id, landmark)
+                h, w, c = img.shape
+                cx, cy = int(lm.x*w), int(lm.y*h)
+                print(id, cx, cy)
+                if id == 0:
+                    cv2.circle(img, (cx,cy), 10 ,(255,0,255), cv2.FILLED)
+
             mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
 
+    # print Frame Rate
     cTime = time.time()
-
     fps = 1 / (cTime - pTime)
     pTime = cTime
-
     cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 2)
 
     cv2.imshow('Image', img)
