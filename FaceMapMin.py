0b40c053a5b9f27ee6f2c99d35b32768d82eeff8
giova86 <giovanni.bocchi@gmail.com>
2021-11-17 13:44:08 +0100

    face mesh

--- FaceMapMin.py
+++ FaceMapMin.py
@@ -18,6 +18,7 @@ with mp_face_mesh.FaceMesh(
     min_tracking_confidence=0.5) as face_mesh:
   while cap.isOpened():
     success, image = cap.read()
+
     if not success:
         print("Ignoring empty camera frame.")
         # If loading a video, use 'break' instead of 'continue'.
